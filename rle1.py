2b108fe19ad19ed76a9f867d18806355945bc4f8
Travis Brady <travis@mealworm.(none)>
2009-03-27 00:57:57 -0400

    avoid .strip() call until necessary

--- rle1.py
+++ rle1.py
@@ -5,14 +5,14 @@ def main():
     this_len,this_start = 1,0
     lastline = stdin.readline().strip()
     for i,line in enumerate(stdin):
-        line = line.strip()
+        #line = line.strip()
         if line != lastline:
-            print "%s,%d,%d" % (lastline, this_start, this_len)
+            print "%s,%d,%d" % (lastline.strip(), this_start, this_len)
             this_start, this_len = i+1, 1
         else:
             this_len += 1
         lastline = line
-    print "%s,%d,%d" % (line, this_start, this_len)
+    print "%s,%d,%d" % (line.strip(), this_start, this_len)
 
 if __name__ == '__main__':
     main()
