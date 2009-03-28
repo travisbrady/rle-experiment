3e30d75a1d3966c80e72a27a0da71859307bd2f7
Travis Brady <travis@mealworm.(none)>
2009-03-27 20:37:56 -0400

    add ruby versio

---
+++ rle.rb
@@ -0,0 +1,16 @@
+lastline = STDIN.readline
+this_start = 0
+this_len = 1
+its = 1
+for line in STDIN.readlines
+    if line != lastline then
+        printf("%s,%d,%d\n", lastline.chomp, this_start, this_len)
+        this_start = its
+        this_len = 1
+    else
+        this_len += 1
+    end
+    lastline = line
+    its += 1
+end
+printf("%s,%d,%d\n", lastline.chomp, this_start, this_len)
